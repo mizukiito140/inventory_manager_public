c14ce6ea139426d3986509490ecb9331473a6dba
mizukiito140 <mizuki0410.tsuuyaku@gmail.com>
2026-01-29 09:45:16 +0900

    Add Japanese labels to inventory item fields

--- inventory/models.py
+++ inventory/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class InventoryItem(models.Model):
-    name = models.CharField(max_length=100)
-    expiration_date = models.DateField()
+    name = models.CharField("食品名", max_length=100)
+    expiration_date = models.DateField("賞味期限")
 
     def __str__(self):
         return self.name
