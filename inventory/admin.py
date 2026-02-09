8cb425e72224fc863fa447f01294f9d54e90a093
mizukiito140 <mizuki0410.tsuuyaku@gmail.com>
2026-02-09 19:08:25 +0900

    updated README

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
 from .models import InventoryItem
 # Register your models here.
-# モデルを管理画面に登録
 admin.site.register(InventoryItem)
--- inventory/urls.py
+++ inventory/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import item_list, item_edit, item_delete, recipe_detail, recipe_search
+from .views import item_list, item_edit, item_delete, recipe_search, recipe_detail
 
 app_name = "inventory"
 
@@ -7,10 +7,12 @@ urlpatterns = [
     # 在庫一覧 & アイテム登録
     path("", item_list, name="item_list"),
     
-    path("recipe-search/", recipe_search, name="recipe_search"),
     # アイテム編集・削除
     path("edit/<int:pk>/", item_edit, name="item_edit"),
     path("delete/<int:pk>/", item_delete, name="item_delete"),
+    
+    # レシピ検索
+    path("recipe-search/", recipe_search, name="recipe_search"),
 
     # レシピ詳細
     path("recipe/<int:recipe_id>/", recipe_detail, name="recipe_detail"),
